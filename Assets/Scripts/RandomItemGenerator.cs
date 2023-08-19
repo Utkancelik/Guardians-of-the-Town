0a6c08b81e1fcd982eefcf1ccc547553c1857e9c
Utkan <utkancelik@hacettepe.edu.tr>
2023-08-19 15:30:06 +0300

    Random Item Generator - Items Randomly Selected Now. But Not Added to the Item Slots.

--- Assets/Scripts/RandomItemGenerator.cs
+++ Assets/Scripts/RandomItemGenerator.cs
@@ -4,6 +4,47 @@ using UnityEngine;
 
 public class RandomItemGenerator : MonoBehaviour
 {
-    public List<GameObject> Items = new List<GameObject>();
+    [SerializeField] private List<GameObject> TurkishItemsPrefab = new List<GameObject>();
+    [SerializeField] private List<GameObject> FinnishItemsPrefabs = new List<GameObject>();
+    [SerializeField] private List<GameObject> SelectedItems = new List<GameObject>();
 
+    [SerializeField] private int totalMatchCount = 4;
+    private void Start()
+    {
+        string language = PlayerPrefs.GetString("Language");
+
+        if (language == "Turkish")
+        {
+            ChooseRandomItems(TurkishItemsPrefab);
+        }
+        else if (language == "Finnish")
+        {
+            ChooseRandomItems(FinnishItemsPrefabs);
+        }
+    }
+
+    private void ChooseRandomItems(List<GameObject> ItemsPrefab)
+    {
+        for (int i = 0; i < totalMatchCount; i++)
+        {
+            // 0 ile toplam prefab sayýsý (12) ile arasýnda random bir sayý belirle
+            int randomItem = Random.Range(0, ItemsPrefab.Count);
+            // çýkan sayý indexindeki objeyi tut
+            GameObject selectedItem = ItemsPrefab[randomItem];
+            // bu objeyi ilk eleman olarak selected item listesine ekle
+            SelectedItems.Add(selectedItem);
+            ItemsPrefab.RemoveAt(randomItem);
+            // çýkan sayýnýn indexinde bulunan prefabin ilk harfini al
+            char firstLetter = selectedItem.name[0];
+            // ayný harften bir obje bulana kadar random at
+            int randomItemWithSameFirstLetter = Random.Range(0, ItemsPrefab.Count);
+            while (ItemsPrefab[randomItemWithSameFirstLetter].name[0] != firstLetter && ItemsPrefab[randomItemWithSameFirstLetter] != selectedItem)
+            {
+                randomItemWithSameFirstLetter = Random.Range(0, ItemsPrefab.Count);
+            }
+            GameObject newItemWitheSameFirstLetter = ItemsPrefab[randomItemWithSameFirstLetter];
+            SelectedItems.Add(newItemWitheSameFirstLetter);
+            ItemsPrefab.RemoveAt(randomItemWithSameFirstLetter);
+        }
+    }
 }
